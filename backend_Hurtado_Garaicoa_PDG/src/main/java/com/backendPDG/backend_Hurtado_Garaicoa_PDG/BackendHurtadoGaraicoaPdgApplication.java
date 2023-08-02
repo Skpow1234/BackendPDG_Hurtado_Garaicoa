76e8644ca9658e9ffd626b4bf20e40a3fb0a5f73
Juan Hurtado <jfhvjfhv0015@gmail.com>
2023-08-02 12:01:03 -0500

    Primera instancia proyecto de grado

---
+++ backend_Hurtado_Garaicoa_PDG/src/main/java/com/backendPDG/backend_Hurtado_Garaicoa_PDG/BackendHurtadoGaraicoaPdgApplication.java
@@ -0,0 +1,13 @@
+package com.backendPDG.backend_Hurtado_Garaicoa_PDG;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendHurtadoGaraicoaPdgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendHurtadoGaraicoaPdgApplication.class, args);
+	}
+
+}
---
+++ backend_Hurtado_Garaicoa_PDG/src/test/java/com/backendPDG/backend_Hurtado_Garaicoa_PDG/BackendHurtadoGaraicoaPdgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backendPDG.backend_Hurtado_Garaicoa_PDG;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendHurtadoGaraicoaPdgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
