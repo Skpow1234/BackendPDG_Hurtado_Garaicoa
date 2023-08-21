2daab3649a016c8e90ccd696b8fc5207c707df0e
Juan Hurtado <jfhvjfhv0015@gmail.com>
2023-08-21 12:22:20 -0500

    Correcion de error, que no estaba dejando autenticar usuarios

--- backend_Hurtado_Garaicoa_PDG/src/main/java/com/backendPDG/backend_Hurtado_Garaicoa_PDG/model/entity/User.java
+++ backend_Hurtado_Garaicoa_PDG/src/main/java/com/backendPDG/backend_Hurtado_Garaicoa_PDG/model/entity/User.java
@@ -4,9 +4,11 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import lombok.*;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
+import java.util.List;
 
 @Getter
 @Setter
@@ -41,31 +43,36 @@ public class User implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
+        return List.of(new SimpleGrantedAuthority(userRole.name()));
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
     }
 
     @Override
     public String getUsername() {
-        return null;
+        return email;
     }
 
     @Override
     public boolean isAccountNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isAccountNonLocked() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isCredentialsNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isEnabled() {
-        return false;
+        return true;
     }
 }
